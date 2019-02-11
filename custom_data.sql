35be6ae16034ce50a35dc84d417d2a01e905e884
Régis Haubourg <regis.haubourg@oslandia.com>
2019-02-11 17:09:42 +0100

    adds custom_data.sql

---
+++ custom_data.sql
@@ -0,0 +1,6 @@
+﻿-- Column: custom_data
+
+-- ALTER TABLE qgep_od.wastewater_networkelement DROP COLUMN custom_data;
+
+ALTER TABLE qgep_od.wastewater_networkelement ADD COLUMN custom_data hstore;
+COMMENT ON COLUMN qgep_od.wastewater_networkelement.custom_data IS 'Hstore key / value field to store additional information not fitting into the standard';
